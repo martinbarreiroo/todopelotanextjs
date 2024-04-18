941bec71e6b38633a6f92d756579fb80c47bc7b8
FranciscoLang2209 <francisco.lang2209@gmail.com>
2024-04-18 13:07:42 -0300

    Refactor CrearTorneo component styling and add hover effect to buttons

--- src/components/CrearTorneo/CrearTorneo.jsx
+++ src/components/CrearTorneo/CrearTorneo.jsx
@@ -49,12 +49,15 @@ function CrearTorneo() {
     return (
         <div className="relative flex flex-col items-center justify-center h-screen space-y-4">
             <img src='/assets/logo.png' alt="Logo" className="w-24 h-24 absolute top-1/8 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
-            <button className="absolute top-4 right-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            <button className="absolute top-4 right-4 font-bold py-2 px-4 rounded"
               onClick={() => {
                 window.location.href = '/Hub';
               }}
+              style={{backgroundColor: '#729560'}}
+              onMouseOver={(e) => e.currentTarget.style.backgroundColor = '#abcd99'}
+              onMouseOut={(e) => e.currentTarget.style.backgroundColor = '#729560'}
             >
-              Return to Hub
+                <img src='/assets/hub.png' alt="Return to Hub" className="w-8 h-8 object-cover" />
             </button>
             <div className="w-full flex flex-col items-center justify-between space-y-10 mt-24">
                 <div className="w-80 h-16 flex items-center justify-center mx-auto mt-32 mb-50 p-6 relative rounded" style={{backgroundColor: '#d1d1d1'}}>
@@ -64,7 +67,7 @@ function CrearTorneo() {
                     <input type="text" placeholder='Number of Players' onChange={e => setPlayers(e.target.value)} className="w-full h-full bg-transparent outline-none"/>
                 </div>
                 <div className="w-80 h-16 flex items-center justify-center mx-auto mt-32 mb-50 p-6 relative rounded" style={{backgroundColor: '#d1d1d1'}}>
-                      <select className="w-full h-full bg-transparent outline-none"  onChange={e => setType(e.target.value)}> 
+                      <select className="w-full h-90% bg-transparent outline-none"  onChange={e => setType(e.target.value)}> 
                         {types.map(type => (
                             <option key={type.id} value={type.id}>
                                 {type.name}
@@ -75,8 +78,11 @@ function CrearTorneo() {
                 <div className="w-80 h-16 flex items-center justify-center mx-auto mt-32 mb-50 p-6 relative rounded" style={{backgroundColor: '#d1d1d1'}}>
                     <input type="text" placeholder='Brief description' onChange={e => setDescription(e.target.value)} className="w-full h-full bg-transparent outline-none"/>
                 </div>
-                <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                <button className=" text-white font-bold py-2 px-4 rounded"
                     onClick={() => create(name, players, type, description, router)}
+                    style={{backgroundColor: '#729560'}}
+                    onMouseOver={(e) => e.currentTarget.style.backgroundColor = '#abcd99'}
+                    onMouseOut={(e) => e.currentTarget.style.backgroundColor = '#729560'}
                 > 
                     Create
                 </button>
