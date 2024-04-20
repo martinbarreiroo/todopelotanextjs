78aba90e59d0b79ff41c60720fc0e3f114c9ca30
martinbarreiroo <mbarreiro@mail.austral.edu.ar>
2024-04-20 19:46:04 -0300

    Add favicon to the app and remove unused SVG files

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import '../styles/global.css'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="app-background">
+      <Head>
+        <link rel="icon" href="/favicon/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </div>
   )
