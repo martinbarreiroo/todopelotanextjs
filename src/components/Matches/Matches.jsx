2dbb6e63c18935cac428c85835c78b76aaeb1503
martinbarreiroo <mbarreiro@mail.austral.edu.ar>
2024-07-14 17:09:30 -0300

    feat: Adjust scale factor for image in Matches component

--- src/components/Matches/Matches.jsx
+++ src/components/Matches/Matches.jsx
@@ -84,7 +84,7 @@ function Matches() {
           pdf.text(title, titleX, 20, { align: "center" });
 
           // Calculate scale factor
-          const scaleFactor = 0.1; // Adjust this value to change the scale
+          const scaleFactor = 0.19; // Adjust this value to change the scale
 
           let imgWidth = canvas.width * scaleFactor;
           let imgHeight = canvas.height * scaleFactor;
